2814f86413aee214eb19b734418031dede015042
Notarin <424c414e4b@gmail.com>
2019-01-29 22:20:57 -0500

    msg > message and moved help

--- index.js
+++ index.js
@@ -14,72 +14,71 @@ client.on("message", message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if(command === 'ping') {
-    message.channel.send('Pong!');
-  } else
-  if (command === 'blah') {
-    message.channel.send('Meh.');
+  if (command === 'help')) {
+    message.reply('say ' + prefix + 'yandere for a random post from yande.re')
+    message.reply('say ' + prefix + 'konachan for a random post from konachan.com')
+    message.reply('say ' + prefix + 'danbooru for a random post from danbooru.donmai.us')
   }
 });
 
-client.on('message', msg => {
-  if (msg.content.startsWith(prefix + 'help')) {
-    msg.reply('say ' + prefix + 'yandere for a random post from yande.re')
-    msg.reply('say ' + prefix + 'konachan for a random post from konachan.com')
-    msg.reply('say ' + prefix + 'danbooru for a random post from danbooru.donmai.us')
+client.on('message', message => {
+  if (message.content.startsWith(prefix + 'help')) {
+    message.reply('say ' + prefix + 'yandere for a random post from yande.re')
+    message.reply('say ' + prefix + 'konachan for a random post from konachan.com')
+    message.reply('say ' + prefix + 'danbooru for a random post from danbooru.donmai.us')
   }
 });
 
-client.on('message', msg => {
-  if (msg.content.startsWith(prefix + 'yandere')) {
-    if (msg.channel.nsfw) {
+client.on('message', message => {
+  if (message.content.startsWith(prefix + 'yandere')) {
+    if (message.channel.nsfw) {
       let rating = "e"
       console.log("NSFW")
     } else {
       let rating = "s"
     }
     console.log("request recieved");
-    msg.reply('request recieved');
+    message.reply('request recieved');
     require("request")("https://yande.re/post.json?limit=1&tags=order%3Arandom+rating%3A" + rating,
       function(err, res, body) {
         let data = JSON.parse(body);
-        msg.channel.send(data['0'].sample_url)
+        message.channel.send(data['0'].sample_url)
       });
   }
 });
 
-client.on('message', msg => {
-  if (msg.content.startsWith(prefix + 'konachan')) {
-    if (msg.channel.nsfw) {
+client.on('message', message => {
+  if (message.content.startsWith(prefix + 'konachan')) {
+    if (message.channel.nsfw) {
       let rating = "e"
       console.log("NSFW")
     } else {
       let rating = "s"
     }
     console.log("request recieved");
-    msg.reply('request recieved');
+    message.reply('request recieved');
     require("request")("https://konachan.com/post.json?limit=1&tags=order%3Arandom+rating%3A" + rating,
       function(err, res, body) {
         let data = JSON.parse(body);
-        msg.channel.send(data['0'].sample_url)
+        message.channel.send(data['0'].sample_url)
       });
   }
 });
 
-client.on('message', msg => {
-  if (msg.content.startsWith(prefix + 'danbooru')) {
-    if (msg.channel.nsfw) {
+client.on('message', message => {
+  if (message.content.startsWith(prefix + 'danbooru')) {
+    if (message.channel.nsfw) {
       let rating = "e"
       console.log("NSFW")
     } else {
       let rating = "s"
     }
     console.log("request recieved");
-    msg.reply('request recieved');
+    message.reply('request recieved');
     require("request")("https://danbooru.donmai.us/posts.json?limit=1&tags=order%3Arandom+rating%3A" + rating,
       function(err, res, body) {
         let data = JSON.parse(body);
-        msg.channel.send(data['0'].file_url)
+        message.channel.send(data['0'].file_url)
       });
   }
 });
