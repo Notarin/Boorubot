43b514cf0690f68b2fbc530deb3c8f2e8cb7ec71
424c414e4b <424c414e4b@gmail.com>
2019-01-30 09:02:54 -0500

    config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "&";
+const conf = require("./config.json");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
